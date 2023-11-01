e74f1b5e92f45c1f3932aac72b028f944bd5c763
Lachheb-Amine <Lachheb.Amine@bcg.com>
2023-11-01 23:49:04 +0100

    p1 completed and tested to be tested more

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 "
-
 apk update && apk upgrade && apk add curl
+
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1"
 
 curl -sfL https://get.k3s.io |  sh -
 
--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-# Set up the initial variables
-NAME=$3
-TOKEN_FILE="/vagrant/scripts/node-token"
-INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
-
-# Check if TOKEN_FILE exists
-if [[ ! -e $TOKEN_FILE ]]; then
-  echo "Error: Token file does not exist at $TOKEN_FILE"
-  exit 1
-fi
-
 # Install curl if not available
 apk update && apk upgrade && apk add curl
 
-# Attempt to download and run the installation script
-if ! curl -sfL https://get.k3s.io | sh -; then
-  echo "Error installing k3s"
-  exit 1
-fi
+# Set up the initial variables
+NAME=$3
+export TOKEN_FILE="/vagrant/scripts/node-token"
+export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
+
+# download and run the installation script
+curl -sfL https://get.k3s.io | sh -
 
 # Setup alias for all users
 echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
