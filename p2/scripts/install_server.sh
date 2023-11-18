dca27cf135b4eea6411b75213ee2d47692bfd82a
Lachheb-Amine <Lachheb.Amine@bcg.com>
2023-11-18 11:10:53 +0100

    p2 scratchs

---
+++ p2/scripts/install_server.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apk update && apk upgrade && apk add curl
+
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1"
+
+curl -sfL https://get.k3s.io |  sh -
+
+# Wait for the k3s service to be active
+sleep 10
+
+echo "[INFO]  Successfully installed k3s on server node"
+
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
+
