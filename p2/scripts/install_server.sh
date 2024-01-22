2476e8e7d8ae5c1eaca5556e82b52dad29dd1358
Lachheb-Amine <Lachheb.Amine@bcg.com>
2024-01-22 11:11:16 +0100

    p2

--- p2/scripts/install_server.sh
+++ p2/scripts/install_server.sh
@@ -1,14 +1,32 @@
-#!/bin/sh
-apk update && apk upgrade && apk add curl
+#!/bin/bash
 
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1"
+# Install K3s
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 --no-deploy traefik"
 
-curl -sfL https://get.k3s.io |  sh -
+curl -sfL https://get.k3s.io | sh -
+
+echo "alias k=\"sudo kubectl\"" >> /etc/profile
 
 # Wait for the k3s service to be active
 sleep 10
 
-echo "[INFO]  Successfully installed k3s on server node"
+# Install nginx-ingress controller
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.34.1/deploy/static/provider/cloud/deploy.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.32.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.1/deploy/static/provider/baremetal/deploy.yaml
+# Wait for a few seconds to ensure the ingress controller is up
+sleep 20
 
-echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
+# delete admission webhook
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 
+# Deploy the web applications
+kubectl apply -f /vagrant/confs/app1.yaml
+kubectl apply -f /vagrant/confs/app2.yaml
+kubectl apply -f /vagrant/confs/app3.yaml
+
+# Set up the load balancer
+kubectl apply -f /vagrant/confs/loadbalancer.yaml
+
+# Set up Ingress to route traffic
+kubectl apply -f /vagrant/confs/ingress.yaml
