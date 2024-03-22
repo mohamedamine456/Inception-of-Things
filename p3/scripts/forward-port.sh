fb93a910b886db4be581428292247742da406e4a
AMZIL Ayoub <amzil.ayoob@gmail.com>
2024-03-22 20:41:50 +0100

    [SWITCH] app: v2

--- p3/scripts/forward-port.sh
+++ p3/scripts/forward-port.sh
@@ -6,4 +6,3 @@ operation_title(){
 
 # operation_title "Forward port"
 kubectl port-forward -n argocd svc/argocd-server 8080:443 & kubectl port-forward -n dev svc/playground-service 8888:8888 # after this command, you can access argocd on localhost:8080
-
