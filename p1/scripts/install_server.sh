23168f6b02ecfed5c4061bb4c365d0064cafea18
mohamedamine456 <amine@cashflowpositif.fr>
2024-08-16 22:46:01 +0100

    fix(P1): increasing the sleep time so the k3s is started properly

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -6,7 +6,7 @@ export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --nod
 curl -sfL https://get.k3s.io |  sh -
 
 # Wait for the k3s service to be active
-sleep 10
+sleep 30
 
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/scripts/
 
