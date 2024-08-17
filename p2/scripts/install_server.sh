9df59c3789bdc41796c9ea0a3b7bde97ff8b4dc1
mohamedamine456 <amine@cashflowpositif.fr>
2024-08-17 19:59:47 +0100

    chore(p2): runnings server and three apps

--- p2/scripts/install_server.sh
+++ p2/scripts/install_server.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
+apk update && apk upgrade && apk add curl
 
 # Install K3s
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 --no-deploy traefik"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1"
 
 curl -sfL https://get.k3s.io | sh -
 
-echo "alias k=\"sudo kubectl\"" >> /etc/profile
-
 # Wait for the k3s service to be active
-sleep 10
+sleep 45
 
-# Install nginx-ingress controller
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.34.1/deploy/static/provider/cloud/deploy.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.32.0/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.1/deploy/static/provider/baremetal/deploy.yaml
-# Wait for a few seconds to ensure the ingress controller is up
-sleep 20
-
-# delete admission webhook
-kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+# Ensure kubectl uses the correct config
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 # Deploy the web applications
 kubectl apply -f /vagrant/confs/app1.yaml
 kubectl apply -f /vagrant/confs/app2.yaml
 kubectl apply -f /vagrant/confs/app3.yaml
 
-# Set up the load balancer
-kubectl apply -f /vagrant/confs/loadbalancer.yaml
+while true; do
+    not_running=$(kubectl get pods --no-headers | grep Running | wc -l)
+    if [ "$not_running" -eq 5 ]; then
+        echo "[INFO]: All pods are running"
+        break
+    else
+        echo "[INFO] Some pods are not running. waiting ..."
+        sleep 10
+    fi
+done
 
 # Set up Ingress to route traffic
 kubectl apply -f /vagrant/confs/ingress.yaml
+
+sleep 30
+
+while true; do
+    endpoints_running=$(kubectl get endpoints app-one app-two app-three --no-headers | grep '<none>' | wc -l)
+    if [ "$endpoints_running" -eq 0 ]; then
+        echo "[INFO]: All endpoints are running"
+        break
+    else
+        echo "[INFO] Some endpoints are not running. waiting ..."
+        sleep 10
+    fi
+done
+
+sleep 120
+
+echo "alias k=\"sudo kubectl\"" >> /etc/profile
+echo "[INFO]  Successfully installed k3s on server node"
